16c6a8137179152b19ddef7d64a95cfecb4264a8
romz987pc <eesistance@gmail.com>
2024-06-15 12:54:58 +0300

    info method

--- calculon_app/calculator/logs.py
+++ calculon_app/calculator/logs.py
@@ -1,9 +1,23 @@
 import logging
 from colorama import init, Fore, Style 
 
+# Уровень логирования
+logging.basicConfig(level=logging.INFO)
+# Инициализируем coloram-у
+init(autoreset=True)
+
 
 class LogClass:
 
 
     def __init__(self):
         pass
+
+
+    def info_message(self, mesg):
+        """
+        Log info message 
+        """
+        logging.info(
+            f'{Fore.YELLOW}[info]{mesg}{Style.RESET_ALL}'
+        )
